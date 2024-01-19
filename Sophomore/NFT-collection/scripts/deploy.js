de23f15eee80222cde4e9fd2c6be71ece9593cfd
Raad05 <ashabulraad@gmail.com>
2024-01-19 15:51:05 +0600

    deploy script

---
+++ Sophomore/NFT-collection/scripts/deploy.js
@@ -0,0 +1,33 @@
+const { ethers, run } = require("hardhat");
+
+const sleep = async (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const main = async () => {
+  const contract = await ethers.deployContract("Whitelist", [10]);
+  console.log("Deploying contract...");
+
+  // wait for deployment
+  await contract.waitForDeployment();
+
+  const contractAddress = contract.target;
+
+  console.log("Whitelist contract address:", contractAddress);
+
+  // Sleep for 30 seconds while Etherscan indexes the new contract deployment
+  await sleep(30 * 1000);
+  console.log("Contract deployed successfully.");
+
+  // Verify the contract on etherscan
+  //   await run("verify:verify", {
+  //     address: contractAddress,
+  //     constructorArguments: [10],
+  //   });
+};
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
